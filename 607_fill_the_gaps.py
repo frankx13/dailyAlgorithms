85b07eab8654056e0ee5a5ad56b9a68efc3f0eb1
frankx13 <frankx13580@gmail.com>
2020-12-01 19:51:30 +0100

    Code issue 612 - Added problem

--- 607_fill_the_gaps.py
+++ 607_fill_the_gaps.py
@@ -12,6 +12,11 @@ def organize_sitting(room_plan_list):
     pointer_holes = []
     pointer_people = []
 
+    consecutive_holes = []
+
+    previous_hole = 0
+
+    # Create a structure representing the room as an integer list, where 0 are holes and 1 are people
     for element in room_plan_list:
         if element == 0:
             pointer_holes.append(indexer)
@@ -19,9 +24,31 @@ def organize_sitting(room_plan_list):
             pointer_people.append(indexer)
         indexer += 1
 
-
     print(pointer_holes)
     print(pointer_people)
+    print(indexer)  # Represents the number of holes + people at beginning
+
+    # Remove holes outside of the leftmost and rightmost people in the room
+    for hole in pointer_holes:
+        if hole < pointer_people[0] or hole > pointer_people[-1]:
+            pointer_holes.remove(hole)
+
+    print(pointer_holes)
+
+    number_holes = len(pointer_holes)
+    number_people = len(pointer_people)
+    print(number_holes)
+    print(number_people)
+
+    for hole in pointer_holes:
+        if hole != pointer_holes[0]:
+            if hole > 0:
+                previous_hole = pointer_holes[hole - 1]
+                if hole == previous_hole:
+                    consecutive_holes.append(previous_hole)
+                    consecutive_holes.append(hole)
+
+    print(consecutive_holes)
 
 
 # n = 7, m = 9
